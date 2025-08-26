52c353c772daa0489bfedd76547b9f1450a3577b
riyasy <riyas.y@gmail.com>
2025-08-26 23:38:55 +0530

    streamlined dependencies. Version upped to 2.5.3

--- Src/FlyPhotos/Readers/SvgReader.cs
+++ Src/FlyPhotos/Readers/SvgReader.cs
@@ -5,10 +5,8 @@ using NLog;
 using SkiaSharp;
 using Svg.Skia;
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
-
 
 
 namespace FlyPhotos.Readers;
@@ -65,65 +63,4 @@ internal class SvgReader
             return (false, null);
         }
     }
-
-
-
-    //private static async Task<(bool, DisplayItem)> LoadSvgRawPixel(CanvasControl ctrl, string inputPath, int width, int height)
-    //{
-    //    try
-    //    {
-    //        var stopwatch = Stopwatch.StartNew();
-
-    //        var svg = new SKSvg();
-    //        svg.Load(inputPath);
-
-    //        var imageInfo = new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
-    //        using var surface = SKSurface.Create(imageInfo);
-    //        var canvas = surface.Canvas;
-
-    //        canvas.Clear(SKColors.Transparent);
-
-    //        var scaleX = (float)width / svg.Picture.CullRect.Width;
-    //        var scaleY = (float)height / svg.Picture.CullRect.Height;
-    //        var scale = Math.Min(scaleX, scaleY);
-
-    //        canvas.Scale(scale);
-    //        canvas.DrawPicture(svg.Picture);
-    //        canvas.Flush();
-
-    //        // --- START OF THE CORRECTED FIX ---
-
-    //        byte[] pixelBytes;
-    //        // Get a pixmap which is a wrapper around the raw pixel data of the surface.
-    //        // For a raster surface like this, it's a fast, zero-copy operation to get the pixmap.
-    //        using (var pixmap = surface.PeekPixels())
-    //        {
-    //            // Get the pixel data as a read-only span and then copy it to a new byte array.
-    //            // This is the one memory copy we need to make to move the data from Skia's
-    //            // memory into a managed array that Win2D can consume.
-    //            pixelBytes = pixmap.GetPixelSpan().ToArray();
-    //        }
-
-    //        // Create a CanvasBitmap directly from the raw byte array. This is a very fast memory copy.
-    //        var canvasBitmap = CanvasBitmap.CreateFromBytes(
-    //            ctrl,
-    //            pixelBytes,
-    //            width,
-    //            height,
-    //            Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized, // Must match SKColorType.Bgra8888
-    //            96); // DPI
-
-    //        // --- END OF THE CORRECTED FIX ---
-
-    //        stopwatch.Stop();
-    //        Debug.WriteLine($"[PERF] LoadSvgRawPixel took: {stopwatch.ElapsedMilliseconds} ms for '{Path.GetFileName(inputPath)}'");
-
-    //        return (true, new DisplayItem(canvasBitmap, PreviewSource.FromDisk));
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        Logger.Error(ex);
-    //        return (false, null);
-    //    }
-    //}
 }
