a667f547a1b40388cb331bb2861716999e1c8aec
RYF <riyas.y@gmail.com>
2026-07-16 09:34:08 +0530

    Missed an underscore When navigating fast, Fly will keep displaying HQ if available. Earlier Fly will display only low-res until high speed navigation is over.
    Refs #213

--- Src/FlyPhotos/Display/Controllers/PhotoDisplayController.cs
+++ Src/FlyPhotos/Display/Controllers/PhotoDisplayController.cs
@@ -159,7 +159,7 @@ internal partial class PhotoDisplayController
     {
         if (_photoList.GetPhoto(key) is not { } photo) return;
 
-        if (cache.IsHqLoaded(key))
+        if (_cache.IsHqLoaded(key))
             await SetSourceAsync(photo, DisplayLevel.Hq);
         else if (_cache.IsPreviewLoaded(key))
             await SetSourceAsync(photo, DisplayLevel.Preview);
